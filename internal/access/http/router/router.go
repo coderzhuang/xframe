bf13d8ffb0220155a57132763e90842493730ba7
zhuangdaxin <zhuangdaxin@morefungroup.com>
2022-05-08 17:20:06 +0800

    完善启动过程

--- internal/access/http/router/router.go
+++ internal/access/http/router/router.go
@@ -2,9 +2,6 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	swaggerfiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
-	"xframe/internal/access/http/handler/common"
 	handlerGoods "xframe/internal/access/http/handler/goods"
 	"xframe/pkg/provider/http_service"
 )
@@ -13,13 +10,6 @@ func InitRoute(
 	HandlerGoods *handlerGoods.HandlerGoods,
 ) http_service.ControllerClosure {
 	return func(e *gin.Engine) {
-		e.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
-
-		commonGroup := e.Group("/")
-		{
-			commonGroup.GET("/version", common.Version)
-		}
-
 		goodsGroup := e.Group("/goods")
 		{
 			goodsGroup.POST("/", HandlerGoods.Add)
--- pkg/provider/http_service/wrap.go
+++ pkg/provider/http_service/wrap.go
@@ -2,8 +2,11 @@ package http_service
 
 import (
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"xframe/config"
 	"xframe/internal/access/http/middleware"
+	"xframe/pkg/common"
 )
 
 type ControllerClosure func(r *gin.Engine)
@@ -12,6 +15,14 @@ func NewRouter(fn ControllerClosure) *gin.Engine {
 	gin.SetMode(config.Conf.Server.Mode)
 	e := gin.New()
 	_ = e.SetTrustedProxies(config.Conf.Server.TrustedProxies)
+
+	e.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+	e.GET("/version", func(c *gin.Context) {
+		common.ResponseSuc(c, map[string]string{
+			"BuildVersion": config.BuildVersion,
+			"BuildAt":      config.BuildAt,
+		})
+	})
 
 	e.Use(gin.Recovery())
 	e.Use(middleware.Exception)
